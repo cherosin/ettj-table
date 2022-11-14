cacdc0a584f3d6b48928f679ddba69b6fbd6d99f
cherosin <60780447+cherosin@users.noreply.github.com>
2022-11-14 10:51:13 -0300

    so o ETTJ IPCA

--- ettj-scraper.py
+++ ettj-scraper.py
@@ -6,33 +6,30 @@ url = 'https://www.anbima.com.br/informacoes/est-termo/CZ.asp'
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser')
 
-# tabela do ettj
 ettjDiv = soup.find('div',{'id':'ETTJs'})
 table = ettjDiv.find('table',{'border':'1'})
 
-# dia
 dadosDiv = soup.find('div',{'id':'Dados'})
 diaTh = dadosDiv.find('th')
 
-tableHeaders = ['Vertices', 'ETTJ IPCA', 'ETTJ PRE', 'Inflacao Implicita', 'Data']
-
-# removendo isso porcausa dos acentos, nao quero 
-# importar uma lib nova so pra tirar unicode
-
-#for x in table.find_all('th')[1:]:
-#    title = x.text.strip()
-#    table_headers.append(title)
-
+tableHeaders = ['Vertices', 'ETTJ IPCA', 'ETTJ PRE', 'Inflacao Implicita']
 tableData = []
 
 for row in table.find_all('tr')[2:]:
     data = row.find_all('td')
     rowData = [td.text.strip() for td in data]
-    rowData.append(diaTh.text)
-    tableData.append(rowData)
+    tableData.append(rowData) 
 
-#juntando os titulos com as info
 resultado = [dict(zip(tableHeaders, t)) for t in tableData]
 
+titulos = ['tax', 'value', 'date']
+informacoes = []
+
+for x in resultado:
+    informacoes.append(['ETTJ IPCA', x['ETTJ IPCA'], diaTh.text + ' + ' + x['Vertices']])
+
+resultadoFinal = [dict(zip(titulos, t)) for t in informacoes]
+
 with open('table.json', 'w') as f:
-    json.dump(resultado, f)
+    json.dump(resultadoFinal, f)
+    
